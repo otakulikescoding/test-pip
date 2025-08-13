f1ab6b2cb4261014b0c50a26ffae0725a5dfa268
otakulikescoding <107459141+otakulikescoding@users.noreply.github.com>
2025-08-13 10:32:44 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello-pkg",
+    version="0.1.0",
+    description="A simple hello package",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
